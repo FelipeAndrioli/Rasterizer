8038312fac08c14c081ce50f77a3c70cc02c1635
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-04-17 19:59:23 -0300

    Added pixel inteporlation using barycentric weights

--- src/main.c
+++ src/main.c
@@ -20,8 +20,13 @@ vec2_t vertices[5] = {
     {40, 40},
     {80, 40},
     {40, 80},
-    {90, 90},
-    {72, 20}
+    {90, 90}
+};
+
+color_t colors[3] = {
+    {0xFF, 0x00, 0x00},
+    {0x00, 0xFF, 0x00},
+    {0x00, 0x00, 0xFF}
 };
 
 void process_input(void) {
@@ -55,7 +60,7 @@ int edge_cross(vec2_t *a, vec2_t *b, vec2_t *p) {
     return ab.x * ap.y - ab.y * ap.x;
 }
 
-void fill_triangle(vec2_t v0, vec2_t v1, vec2_t v2, uint32_t color) {
+void fill_triangle(vec2_t v0, vec2_t v1, vec2_t v2) {
     int x_min = MIN(MIN(v0.x, v1.x), v2.x);
     int y_min = MIN(MIN(v0.y, v1.y), v2.y);
     int x_max = MAX(MAX(v0.x, v1.x), v2.x);
@@ -64,6 +69,8 @@ void fill_triangle(vec2_t v0, vec2_t v1, vec2_t v2, uint32_t color) {
     int bias0 = is_top_left(&v1, &v2) ? 0 : -1;
     int bias1 = is_top_left(&v2, &v0) ? 0 : -1;
     int bias2 = is_top_left(&v0, &v1) ? 0 : -1;
+
+    float parallelogram_area = edge_cross(&v0, &v1, &v2);
 
     for (int y = y_min; y <= y_max; y++) {
         for (int x = x_min; x <= x_max; x++) {
@@ -76,7 +83,22 @@ void fill_triangle(vec2_t v0, vec2_t v1, vec2_t v2, uint32_t color) {
             bool is_inside = w0 >= 0 && w1 >= 0 && w2 >= 0;
 
             if (is_inside) {
-                draw_pixel(x, y, color);
+                float alpha = w0 / parallelogram_area;
+                float beta = w1 / parallelogram_area;
+                float gamma = w2 / parallelogram_area;
+
+                int a = 0xFF;
+                int r = (alpha) * colors[0].r + (beta) * colors[1].r + (gamma) * colors[2].r;
+                int g = (alpha) * colors[0].g + (beta) * colors[1].g + (gamma) * colors[2].g;
+                int b = (alpha) * colors[0].b + (beta) * colors[1].b + (gamma) * colors[2].b;
+
+                uint32_t interpolated_color = 0x000000;
+                interpolated_color = (interpolated_color | a) << 8;
+                interpolated_color = (interpolated_color | r) << 8;
+                interpolated_color = (interpolated_color | g) << 8;
+                interpolated_color = (interpolated_color | b);
+
+                draw_pixel(x, y, interpolated_color);
             }
         }
     }
@@ -89,11 +111,9 @@ void render(void) {
     vec2_t vec1 = vertices[1];
     vec2_t vec2 = vertices[2];
     vec2_t vec3 = vertices[3];
-    vec2_t vec4 = vertices[4];
 
-    fill_triangle(vec0, vec1, vec2, 0xFF00FF00);
-    fill_triangle(vec3, vec2, vec1, 0xFFA74DE3);
-    fill_triangle(vec4, vec1, vec0, 0xFF0390FC);
+    fill_triangle(vec0, vec1, vec2);
+    fill_triangle(vec3, vec2, vec1);
 
     render_framebuffer();
 }
