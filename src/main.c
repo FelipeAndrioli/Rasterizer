e0325e4fb2ff52177bae04aedce787090351046e
Felipe Andrioli <felipeandrioli56@hotmail.com>
2023-04-17 20:58:06 -0300

    Added subpixel rasterization; Added rotation to the triangles

--- src/main.c
+++ src/main.c
@@ -53,7 +53,7 @@ bool is_top_left(vec2_t *start, vec2_t *end) {
     return is_top_edge || is_left_edge;
 }
 
-int edge_cross(vec2_t *a, vec2_t *b, vec2_t *p) {
+float edge_cross(vec2_t *a, vec2_t *b, vec2_t *p) {
     vec2_t ab = {b->x - a->x, b->y - a->y};
     vec2_t ap = {p->x - a->x, p->y - a->y};
 
@@ -61,29 +61,29 @@ int edge_cross(vec2_t *a, vec2_t *b, vec2_t *p) {
 }
 
 void fill_triangle(vec2_t v0, vec2_t v1, vec2_t v2) {
-    int x_min = MIN(MIN(v0.x, v1.x), v2.x);
-    int y_min = MIN(MIN(v0.y, v1.y), v2.y);
-    int x_max = MAX(MAX(v0.x, v1.x), v2.x);
-    int y_max = MAX(MAX(v0.y, v1.y), v2.y);
+    int x_min = floor(MIN(MIN(v0.x, v1.x), v2.x));
+    int y_min = floor(MIN(MIN(v0.y, v1.y), v2.y));
+    int x_max = ceil(MAX(MAX(v0.x, v1.x), v2.x));
+    int y_max = ceil(MAX(MAX(v0.y, v1.y), v2.y));
 
-    int delta_w0_col = (v1.y - v2.y);
-    int delta_w1_col = (v2.y - v0.y);
-    int delta_w2_col = (v0.y - v1.y);
+    float delta_w0_col = (v1.y - v2.y);
+    float delta_w1_col = (v2.y - v0.y);
+    float delta_w2_col = (v0.y - v1.y);
 
-    int delta_w0_row = (v2.x - v1.x);
-    int delta_w1_row = (v0.x - v2.x);
-    int delta_w2_row = (v1.x - v0.x);
+    float delta_w0_row = (v2.x - v1.x);
+    float delta_w1_row = (v0.x - v2.x);
+    float delta_w2_row = (v1.x - v0.x);
 
-    int bias0 = is_top_left(&v1, &v2) ? 0 : -1;
-    int bias1 = is_top_left(&v2, &v0) ? 0 : -1;
-    int bias2 = is_top_left(&v0, &v1) ? 0 : -1;
+    float bias0 = is_top_left(&v1, &v2) ? 0 : -0.0001;
+    float bias1 = is_top_left(&v2, &v0) ? 0 : -0.0001;
+    float bias2 = is_top_left(&v0, &v1) ? 0 : -0.0001;
 
     float parallelogram_area = edge_cross(&v0, &v1, &v2);
 
-    vec2_t p0 = {x_min, y_min};
-    int w0_row = edge_cross(&v1, &v2, &p0) + bias0;
-    int w1_row = edge_cross(&v2, &v0, &p0) + bias1;
-    int w2_row = edge_cross(&v0, &v1, &p0) + bias2;
+    vec2_t p0 = {x_min + 0.5f, y_min + 0.5f};
+    float w0_row = edge_cross(&v1, &v2, &p0) + bias0;
+    float w1_row = edge_cross(&v2, &v0, &p0) + bias1;
+    float w2_row = edge_cross(&v0, &v1, &p0) + bias2;
 
     for (int y = y_min; y <= y_max; y++) {
         float w0 = w0_row;
@@ -123,11 +123,14 @@ void fill_triangle(vec2_t v0, vec2_t v1, vec2_t v2) {
 
 void render(void) {
     clear_framebuffer(0xFF000000);
-   
-    vec2_t vec0 = vertices[0];
-    vec2_t vec1 = vertices[1];
-    vec2_t vec2 = vertices[2];
-    vec2_t vec3 = vertices[3];
+  
+    float angle = SDL_GetTicks() / 1000.0f * 0.5f;
+    vec2_t center = {SCREEN_WIDTH / 2.0f, SCREEN_HEIGHT / 2.0f};
+
+    vec2_t vec0 = vec2_rotate(vertices[0], center, angle);
+    vec2_t vec1 = vec2_rotate(vertices[1], center, angle);
+    vec2_t vec2 = vec2_rotate(vertices[2], center, angle);
+    vec2_t vec3 = vec2_rotate(vertices[3], center, angle);
 
     fill_triangle(vec0, vec1, vec2);
     fill_triangle(vec3, vec2, vec1);
